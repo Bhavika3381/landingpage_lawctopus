cce2125ad23af521c700c98920c13a41ff7463c0
bhavikatambe-nfd <bhavika.tambe@nexflaredynamics.com>
2026-06-27 17:37:50 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
-import Courses from "./Pages/Courses";
+import Courses from "./Components/Courses/Courses";
 import About from "./Pages/About";
-import Opportunities from "./Pages/Opportunities";
+import Opportunities from "./Components/Opportunities/Opportunities";
 
 function App() {
   return (
